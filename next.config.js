858bfc2f61f11a60e8f64e661ebcd9e569f9f24e
Estela <diazsanchez.estela@gmail.com>
2018-05-11 12:56:13 -0700

    npm git generic quickpush

--- next.config.js
+++ next.config.js
@@ -1,22 +1,16 @@
-const withCSS = require('@zeit/next-css')
-
-
-module.exports = withCSS({
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/p/hello-nextjs': {page: '/post', query: { title: "Hello Next.js" } },
-      '/p/learn-nextjs': { page: '/post', query: { title: "Learn Next.js is awesome" } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: "Deploy apps with Zeit" } },
-      '/p/exporting-pages': { page: '/post', query: { title: "Learn to Export HTML Pages" } }
-    }
-  }
-})
-
 const webpack = require('webpack')
 
-module.exports = {
+module.exports = withCSS({
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/p/hello-nextjs': {page: '/post', query: { title: "Hello Next.js" } },
+        '/p/learn-nextjs': { page: '/post', query: { title: "Learn Next.js is awesome" } },
+        '/p/deploy-nextjs': { page: '/post', query: { title: "Deploy apps with Zeit" } },
+        '/p/exporting-pages': { page: '/post', query: { title: "Learn to Export HTML Pages" } }
+      }
+    },
     webpack: (config, { dev }) => {
         config.plugins.push(
             new webpack.ProvidePlugin({
@@ -26,4 +20,4 @@ module.exports = {
         )
         return config
     }
-}
+})
